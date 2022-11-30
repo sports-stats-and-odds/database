5d44103528723ba9178bd3a1975b1beb424626e5
Matteo DE VITA PC <matteo.de-vita@epitech.eu>
2022-11-30 14:01:21 +0100

    update

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.1',
     packages=['connector'],
     package_data={'': ['*.sql']},
+    include_package_data=True,
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
     url='https://github.com/sports-stats-and-odds/database',
