284a579a1aa440b48f660aed1382c48495462b9e
Matteo DE VITA PC <matteo.de-vita@epitech.eu>
2022-11-30 12:39:26 +0100

    add setup.py and __init__.py to trying make it a installable module

---
+++ connector/__init__.py
@@ -0,0 +1,2 @@
+from my_sql_connector import MySQLConnector
+from table_enum import Table
---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='database',
+    version='0.1',
+    packages=['connector',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+    url='https://github.com/sports-stats-and-odds/database'
+)
